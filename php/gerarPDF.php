2a743429df3db02ac6b3e14200a8beaec54e5195
MARCEL LEITE DE FARIAS <marcel_farias@estudante.sesisenai.org.br>
2022-06-08 20:57:05 -0300

    pdf

--- php/gerarPDF.php
+++ php/gerarPDF.php
@@ -1,13 +1,49 @@
 <?php
 // Require composer autoload
-require_once __DIR__ . '/vendor/autoload.php';
+require_once("../vendor/autoload.php");
+require_once("conexaoBanco.php");
+
+$idCompromisso = $_POST['idComp'];
+$comando = "SELECT * FROM compromissos WHERE idCompromisso=".$idCompromisso;
+$resultado = mysqli_query($conexao, $comando);
+$comp = mysqli_fetch_assoc($resultado);
 // Create an instance of the class:
 $mpdf = new \Mpdf\Mpdf();
 
 // Write some HTML code:
-$mpdf->WriteHTML('Hello World');
+$mpdf->WriteHTML('<h1> Informação de compromisso</h1>');
+$mpdf->WriteHTML('<hr>');
 
-// Output a PDF file directly to the browser
-$mpdf->Output();
+$mpdf->WriteHTML('<h3>Dados Gerais</h3>');
+$mpdf->WriteHTML('<p>Descrição: '.$comp['descricao'].'</p>');
+$mpdf->WriteHTML('<p>Data Início: '. $comp['dataInicio'].'</p>');
+$mpdf->WriteHTML('<p>Hora: '.$comp['hora'].'</p>');
+$mpdf->WriteHTML('<p>Data Finalização: '.$comp['dataFim'].' </p> <br>');
+
+$mpdf->WriteHTML('<h3>Localização</h3>');
+$mpdf->WriteHTML('<p>Local: '. $comp['local'].'</p>');
+$mpdf->WriteHTML('<p>Rua: '.$comp['rua'].'</p>');
+$mpdf->WriteHTML('<p>Número: '.$comp['numero'].' </p>');
+$mpdf->WriteHTML('<p>Bairro: '.$comp['bairro'].' </p>');
+$mpdf->WriteHTML('<p>Cidade: '.$comp['cidade'].' </p> <br>');
+
+
+
+$mpdf->WriteHTML('<h3>Pessoas</h3>');
+$comando="SELECT p.nome, p.sobrenome FROM pessoas p INNER JOIN compromissos_pessoas cp ON p.idPessoa=cp.pessoas_idPessoa WHERE compromissos_idCompromisso=".$idCompromisso;
+
+    $resultado=mysqli_query($conexao,$comando);
+
+    $pessoas=array();
+
+    while($p = mysqli_fetch_assoc($resultado)){
+        array_push($pessoas,$p);
+    }
+    foreach($pessoas as $p){
+        $mpdf->WriteHTML("<p>".$p['nome']." ".$p['sobrenome']."</p>");
+    }
+    $mpdf->Output();
 
 ?>
+</body>
+</html>
