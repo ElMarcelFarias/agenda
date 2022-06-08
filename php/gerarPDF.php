601d9a9e23d2b8599862a41495cd017c42254cda
MARCEL LEITE DE FARIAS <marcel_farias@estudante.sesisenai.org.br>
2022-06-08 19:44:47 -0300

    second commit!

---
+++ php/gerarPDF.php
@@ -0,0 +1,13 @@
+<?php
+// Require composer autoload
+require_once __DIR__ . '/vendor/autoload.php';
+// Create an instance of the class:
+$mpdf = new \Mpdf\Mpdf();
+
+// Write some HTML code:
+$mpdf->WriteHTML('Hello World');
+
+// Output a PDF file directly to the browser
+$mpdf->Output();
+
+?>
